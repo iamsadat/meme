da0d1d623c01ab269f52e22c523027c847e21599
Sadat <100314833+iamsadat@users.noreply.github.com>
2023-05-22 01:47:51 +0530

    Added the repo link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,17 +5,25 @@ export default function Footer() {
     <div className="footer">
       <p className="credit">© 2023 Meme Generator</p>
       <p>
-        <a href="" className="social">
+        <a
+          href="https://github.com/iamsadat/meme"
+          target="_blank"
+          className="social"
+        >
           Source Code
         </a>
       </p>
       <p>
-        <a href="https://twitter.com/sxdatt" className="social">
+        <a href="https://twitter.com/sxdatt" target="_blank" className="social">
           Twitter
         </a>
       </p>
       <p>
-        <a href="https://github.com/iamsadat" className="social">
+        <a
+          href="https://github.com/iamsadat"
+          target="_blank"
+          className="social"
+        >
           GitHub
         </a>
       </p>
